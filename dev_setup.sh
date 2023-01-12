13e96206c3337c1eebdfbb2f15371604294f5991
chLi <565353780@qq.com>
2023-01-12 19:50:43 +0800

    add setup bash files

---
+++ dev_setup.sh
@@ -0,0 +1,8 @@
+cd ..
+git clone git@github.com:565353780/scan2cad-dataset-manage.git
+
+cd scan2cad-dataset-manage
+./dev_setup.sh
+
+pip install numpy open3d tqdm
+
---
+++ setup.sh
@@ -0,0 +1,8 @@
+cd ..
+git clone https://github.com/565353780/scan2cad-dataset-manage.git
+
+cd scan2cad-dataset-manage
+./setup.sh
+
+pip install numpy open3d tqdm
+
