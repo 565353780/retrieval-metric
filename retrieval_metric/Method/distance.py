c5d9ed68b72bb3cd48b28fae1058a69ee96f02bf
chLi <565353780@qq.com>
2023-01-12 23:24:45 +0800

    finish compute pose errors

--- retrieval_metric/Method/distance.py
+++ retrieval_metric/Method/distance.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import open3d as o3d
 
 
 def getChamferDistance(pcd_1, pcd_2):
@@ -14,3 +15,59 @@ def getChamferDistance(pcd_1, pcd_2):
     chamfer_distance = dist_1 * dist_1 / dist_1_array.shape[
         0] + dist_2 * dist_2 / dist_2_array.shape[0]
     return chamfer_distance
+
+
+def getTransError(pcd_1, pcd_2):
+    center_1 = pcd_1.get_center()
+    center_2 = pcd_2.get_center()
+    center_dist = np.linalg.norm(center_1 - center_2)
+    return center_dist
+
+
+def getRotateError(pcd_1, pcd_2):
+    obb_1 = pcd_1.get_oriented_bounding_box()
+    obb_2 = pcd_2.get_oriented_bounding_box()
+
+    R_1 = obb_1.R
+    R_2 = obb_2.R
+
+    direction_11 = np.array([1, 0, 0]) @ R_1
+    direction_12 = np.array([0, 1, 0]) @ R_1
+    direction_21 = np.array([1, 0, 0]) @ R_2
+    direction_22 = np.array([0, 1, 0]) @ R_2
+
+    direction_dist_11 = np.min([
+        np.linalg.norm(direction_11 - direction_21),
+        np.linalg.norm(direction_11 + direction_21)
+    ])
+    direction_dist_12 = np.min([
+        np.linalg.norm(direction_11 - direction_22),
+        np.linalg.norm(direction_11 + direction_22)
+    ])
+    direction_dist_21 = np.min([
+        np.linalg.norm(direction_12 - direction_21),
+        np.linalg.norm(direction_12 + direction_21)
+    ])
+    direction_dist_22 = np.min([
+        np.linalg.norm(direction_12 - direction_22),
+        np.linalg.norm(direction_12 + direction_22)
+    ])
+
+    direction_dist = np.min([
+        direction_dist_11 + direction_dist_22,
+        direction_dist_12 + direction_dist_21
+    ]) / 2.0
+    cos_error = np.abs(1 - direction_dist * direction_dist / 2.0)
+    angle_error = np.arccos(cos_error) * 180.0 / np.pi
+    return angle_error
+
+
+def getScaleError(pcd_1, pcd_2):
+    max_point_1 = pcd_1.get_max_bound()
+    min_point_1 = pcd_1.get_min_bound()
+    max_point_2 = pcd_2.get_max_bound()
+    min_point_2 = pcd_2.get_min_bound()
+    scale_1 = max_point_1 - min_point_1
+    scale_2 = max_point_2 - min_point_2
+    scale_dist = np.linalg.norm(scale_1 - scale_2)
+    return scale_dist
--- retrieval_metric/Module/metric_manager.py
+++ retrieval_metric/Module/metric_manager.py
@@ -11,9 +11,10 @@ from scan2cad_dataset_manage.Module.object_model_map_manager import \
     ObjectModelMapManager
 from points_shape_detect.Method.trans import normalizePointArray
 
-from retrieval_metric.Method.retrieval import getOursRetrievalResult
-from retrieval_metric.Method.distance import getChamferDistance
 from retrieval_metric.Method.time import getCurrentTime
+from retrieval_metric.Method.retrieval import getOursRetrievalResult
+from retrieval_metric.Method.distance import \
+    getChamferDistance, getTransError, getRotateError, getScaleError
 
 
 class MetricManager(object):
@@ -106,6 +107,9 @@ class MetricManager(object):
         ret2gt_cd = getChamferDistance(retrieval_cad_pcd, gt_cad_pcd)
         scan2gt_cd = getChamferDistance(object_pcd, gt_cad_pcd)
         scan2ret2gt_cd = scan2ret_cd - scan2gt_cd
+        trans_error = getTransError(object_pcd, retrieval_cad_pcd)
+        rotate_error = getRotateError(object_pcd, retrieval_cad_pcd)
+        scale_error = getScaleError(object_pcd, retrieval_cad_pcd)
 
         #  o3d.visualization.draw_geometries([object_pcd, retrieval_cad_mesh])
 
@@ -113,19 +117,46 @@ class MetricManager(object):
         self.ret2gt_cd_list.append(ret2gt_cd)
         self.scan2gt_cd_list.append(scan2gt_cd)
         self.scan2ret2gt_cd_list.append(scan2ret2gt_cd)
+        self.trans_error_list.append(trans_error)
+        self.rotate_error_list.append(rotate_error)
+        self.scale_error_list.append(scale_error)
+
+        self.summary_writer.add_scalar("Retrieval/scan2ret_cd", scan2ret_cd,
+                                       self.step)
+        self.summary_writer.add_scalar("Retrieval/ret2gt_cd", ret2gt_cd,
+                                       self.step)
+        self.summary_writer.add_scalar("Retrieval/scan2gt_cd", scan2gt_cd,
+                                       self.step)
+        self.summary_writer.add_scalar("Retrieval/scan2ret2gt_cd",
+                                       scan2ret2gt_cd, self.step)
+        self.summary_writer.add_scalar("Pose/trans_error", trans_error,
+                                       self.step)
+        self.summary_writer.add_scalar("Pose/rotate_error", rotate_error,
+                                       self.step)
+        self.summary_writer.add_scalar("Pose/scale_error", scale_error,
+                                       self.step)
 
         mean_scan2ret_cd = np.mean(self.scan2ret_cd_list)
         mean_ret2gt_cd = np.mean(self.ret2gt_cd_list)
         mean_scan2gt_cd = np.mean(self.scan2gt_cd_list)
         mean_scan2ret2gt_cd = np.mean(self.scan2ret2gt_cd_list)
-        self.summary_writer.add_scalar("Retrieval/mean_scan2ret_cd",
+        mean_trans_error = np.mean(self.trans_error_list)
+        mean_rotate_error = np.mean(self.rotate_error_list)
+        mean_scale_error = np.mean(selrf.scale_error_list)
+        self.summary_writer.add_scalar("RetrievalMean/mean_scan2ret_cd",
                                        mean_scan2ret_cd, self.step)
-        self.summary_writer.add_scalar("Retrieval/mean_ret2gt_cd",
+        self.summary_writer.add_scalar("RetrievalMean/mean_ret2gt_cd",
                                        mean_ret2gt_cd, self.step)
-        self.summary_writer.add_scalar("Retrieval/mean_scan2gt_cd",
+        self.summary_writer.add_scalar("RetrievalMean/mean_scan2gt_cd",
                                        mean_scan2gt_cd, self.step)
-        self.summary_writer.add_scalar("Retrieval/mean_scan2ret2gt_cd",
+        self.summary_writer.add_scalar("RetrievalMean/mean_scan2ret2gt_cd",
                                        mean_scan2ret2gt_cd, self.step)
+        self.summary_writer.add_scalar("PoseMean/mean_trans_error",
+                                       mean_trans_error, self.step)
+        self.summary_writer.add_scalar("PoseMean/mean_rotate_error",
+                                       mean_rotate_error, self.step)
+        self.summary_writer.add_scalar("PoseMean/mean_scale_error",
+                                       mean_scale_error, self.step)
         self.step += 1
         return True
 
